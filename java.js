64fa56b961e1d1244ec22ff6a5c505170093d0c9
BashirAn <49612968+BashirAn@users.noreply.github.com>
2019-06-01 09:10:20 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,58 @@
+3)var a=[5,7,4,8];
+Function myfunction a=[a[i],a[i+1],a[i+2],a[n]]{
+min= a[0];
+for(i=1; i<a.length;i++){
+if a[i]<min){
+    min=a[i] }
+}}
+console.log ("min element of array is" a[i]
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+4)var a=[5,7,4,8];
+Function myfunction a=[a[i],a[i+1],a[i+2],a[n]]{
+max= a[0];
+for(i=1; i<a.length;i++){
+if a[i]>max){
+    max=a[i] }
+}}
+
+console.log ("max element of array is" a[i]
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	5) var factorial = function(n) {
+    if(n == 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+ 
+console.log(factorial(num));
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+6)var a=[1,2,5,7,3,9,11,12,9,13]
+Function myfunction a=[a[i],a[i+1],a[i+2],a[n]]{
+int max1=0;
+int max2=0;
+for(int n:nums){
+    if(max1<n){
+        max2=max1;
+        max1=n;
+    }else if(max2<n){
+        max2=n
+    }
+}}
+console.log(max1,max2)
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+7)Var a=[1,2,5,8,3,9,11,12,14];
+Function myfunction a=[a[i],a[i+1],a[i+2],a[n]]{
+For (i=1; i<a.length; i++){
+If a[i]%2==0 , 
+a[i]=odd
+Else 
+   a[i]=even}
+}
+
+console.log (odd)
+console.log(even)
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
